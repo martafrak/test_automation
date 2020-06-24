eca652625fb1ca86b02d6ba80bd1ba1ec0705d17
martafrak <marta.frak94@gmail.com>
2020-06-24 21:42:59 +0200

    added selectors and screenshot

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -15,9 +15,15 @@ test('search testcontroller', async t => {
     //change speed for 1 test: 1 - default speed, 0.1 is helpfull for debugging
     //await t.setTestSpeed(1)
     //await t.wait(4000)
-    await t.click('#search-icon')
-    await t.typeText('#search', "testcontroller")
+    const search_icon = Selector('#search-icon')
+    const search_input = Selector('#search')
+    const article_text = Selector('.post-content').innerText
+
+    //await t.takeScreenshot({ fullPage: true })
+    //await t.takeElementScreenshot(search_input)
+    await t.click(search_icon)
+    await t.typeText(search_input, "testcontroller")
     await t.pressKey('enter')   
     
-    await t.expect(Selector('.post-content').innerText).contains("A test controller object exposes the test API's methods.")
+    await t.expect(article_text).contains("A test controller object exposes the test API's methods.")
 })
